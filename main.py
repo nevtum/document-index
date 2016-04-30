ea34a4875f5ab0dcfc793d93d773f9a0110687b0
Neville Tummon <nt.devs@gmail.com>
2016-04-30 23:35:29 +1000

    calculate hash of each file matching predicate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import fnmatch
 import os
 import sys
+import hashlib
 
 
 def find_files(directory, pattern):
@@ -10,6 +11,23 @@ def find_files(directory, pattern):
                 filename = os.path.join(root, basename)
                 yield filename
 
-if __name__ == '__main__':
-    for filename in find_files(sys.argv[1], '*.doc'):
+def get_md5_hash(filename):
+    BLOCKSIZE = 65536
+    hasher = hashlib.md5()
+    with open(filename, 'rb') as afile:
+        buf = afile.read(BLOCKSIZE)
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = afile.read(BLOCKSIZE)
+    return hasher.hexdigest()
+
+def process(filename):
+    try:
         print("match %s" % filename)
+        print("hash %s" % get_md5_hash(filename))
+    except Exception:
+        pass
+
+if __name__ == '__main__':
+    for filename in find_files(sys.argv[1], sys.argv[2]):
+        process(filename)

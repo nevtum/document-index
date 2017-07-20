db0823f7ebca94dfd14caba179e2e5fbeb5642d9
Neville Tummon <nt.devs@gmail.com>
2017-07-20 15:08:14 +1000

    results being rendered to app

--- app.py
+++ app.py
@@ -7,18 +7,6 @@ from flask import Flask,  render_template, json, request
 
 app = Flask(__name__)
 
-def query_index():
-    ix = open_dir("indexdir")
-    while True:
-        indata = input("Enter query: ")
-        with ix.searcher() as searcher:
-            query = QueryParser("content", ix.schema).parse(indata)
-            results = searcher.search(query)
-            
-            for item in results[:10]:
-                # print(item.score, item['filename'])
-                print(item.score, item['path'])
-
 def to_dict(item):
     return {
         'score': item.score,
@@ -36,7 +24,7 @@ def search():
     ix = open_dir("indexdir")
     with ix.searcher() as searcher:
         query = QueryParser("content", ix.schema).parse(keyword)
-        results = searcher.search(query, limit=50)
+        results = searcher.search(query, limit=25)
         results = map(lambda x: to_dict(x), results)
         response = app.response_class(
             response=json.dumps(list(results)),
@@ -46,7 +34,6 @@ def search():
         return response
 
 def main():
-    # query_index()
     app.run(debug=True)
  
 if __name__ == '__main__':
